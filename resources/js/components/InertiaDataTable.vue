f65648a8839a8ee4762f92b3c727276a07bb0217
HoffmannLuca <luca.hoffmann1000@gmail.com>
2026-03-08 13:39:08 +0100

    create InertiaDataTable component

---
+++ resources/js/components/InertiaDataTable.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { useDataTable } from '@starter-solutions/inertia-data-table-vue';
+import PlaceholderPattern from './PlaceholderPattern.vue';
+
+const props = defineProps<{
+    tableKey: string;
+}>();
+
+const table = useDataTable(props.tableKey);
+</script>
+
+<template>
+    <div>
+        <div class="overflow-x-auto rounded-xl p-4">
+            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div
+                    class="rounded-xl border border-sidebar-border/70 bg-orange-400 p-2 text-sm text-black dark:border-sidebar-border"
+                >
+                    <pre>
+"pagination": {{
+                            {
+                                page: table.pagination.value?.current_page,
+                                perPage: table.pagination.value?.per_page,
+                                sortBy: table.pagination.value?.sort_by,
+                                descending: table.pagination.value?.descending,
+                            }
+                        }}
+                        </pre
+                    >
+                </div>
+                <div
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                >
+                    <PlaceholderPattern />
+                </div>
+                <div
+                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                >
+                    <PlaceholderPattern />
+                </div>
+            </div>
+        </div>
+
+        <div class="h-full overflow-x-auto rounded-xl p-4">
+            <div
+                class="overflow-hidden rounded-xl border border-sidebar-border/70 bg-accent p-2 dark:border-sidebar-border"
+            >
+                <pre>"data": {{ table.data }}</pre>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/pages/Demo.vue
+++ resources/js/pages/Demo.vue
@@ -1,56 +1,12 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
-import { useDataTable } from '@starter-solutions/inertia-data-table-vue';
-
-const userTable = useDataTable('users');
+import InertiaDataTable from '@/components/InertiaDataTable.vue';
 </script>
 
 <template>
     <Head title="Demo" />
 
-    <div id="demo">
-        <div
-            class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
-        >
-            <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div
-                    class="rounded-xl border border-sidebar-border/70 bg-orange-400 p-2 text-black dark:border-sidebar-border"
-                >
-                    <pre>
-"pagination": {{
-                            {
-                                page: userTable.pagination.value?.current_page,
-                                perPage: userTable.pagination.value?.per_page,
-                                sortBy: userTable.pagination.value?.sort_by,
-                                descending:
-                                    userTable.pagination.value?.descending,
-                            }
-                        }}
-                        </pre
-                    >
-                </div>
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
-                >
-                    <PlaceholderPattern />
-                </div>
-                <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
-                >
-                    <PlaceholderPattern />
-                </div>
-            </div>
-        </div>
-
-        <div
-            class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
-        >
-            <div
-                class="rounded-xl border border-sidebar-border/70 bg-accent p-2 dark:border-sidebar-border"
-            >
-                <pre>"data": {{ userTable.data }}</pre>
-            </div>
-        </div>
+    <div id="demo" class="flex h-full gap-2">
+        <InertiaDataTable tableKey="users" />
     </div>
 </template>
