72fa41aee99378b3e8632179d91d5820ebbffa0f
HoffmannLuca <luca.hoffmann1000@gmail.com>
2026-03-08 13:24:42 +0100

    install @starter-solutions/inertia-data-table-vue npm package

--- resources/js/pages/Demo.vue
+++ resources/js/pages/Demo.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+import { useDataTable } from '@starter-solutions/inertia-data-table-vue';
+
+const userTable = useDataTable('users');
 </script>
 
 <template>
@@ -12,9 +15,20 @@ import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
         >
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div
-                    class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                    class="rounded-xl border border-sidebar-border/70 bg-orange-400 p-2 text-black dark:border-sidebar-border"
                 >
-                    <PlaceholderPattern />
+                    <pre>
+"pagination": {{
+                            {
+                                page: userTable.pagination.value?.current_page,
+                                perPage: userTable.pagination.value?.per_page,
+                                sortBy: userTable.pagination.value?.sort_by,
+                                descending:
+                                    userTable.pagination.value?.descending,
+                            }
+                        }}
+                        </pre
+                    >
                 </div>
                 <div
                     class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
@@ -33,9 +47,9 @@ import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
             class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
         >
             <div
-                class="p-2 bg-accent rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
+                class="rounded-xl border border-sidebar-border/70 bg-accent p-2 dark:border-sidebar-border"
             >
-                <pre>"data":{{ $page.props.users }}</pre>
+                <pre>"data": {{ userTable.data }}</pre>
             </div>
         </div>
     </div>
